e3e9692ddefc6e07ec859c4e6da950bede34cf05
Kevin Zheng <jiezheng2020@gmail.com>
2021-02-15 16:35:22 -0600

    phase 2b

--- server.js
+++ server.js
@@ -6,8 +6,28 @@ const server = http.createServer(function(req,res) {
     if (req.url === "/OK") {
         console.log("Inbound 'OK' request being processed...");
         res.writeHead(200);
-    } else {
-        // console.log(req.url)
+    }
+    else if (req.url === "/Bad-Request")  {
+        console.log()
+        res.writeHead(400)
+    }
+    else if (req.url === "/Created")  {
+        console.log('201')
+        res.writeHead(201)
+    }
+    else if (req.url === "/Forbidden")  {
+        console.log('403')
+        res.writeHead(403)
+    }
+    else if (req.url === "/Found")  {
+        console.log('302')
+        res.writeHead(302)
+    }
+    else if (req.url === "/Gateway-Timeout")  {
+        console.log('504')
+        res.writeHead(504)
+    }
+    else {
         console.log("request unable to process")
         res.writeHead(404)
 
